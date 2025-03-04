c4b5da9d9410dd70cb639071a59e51dcc555f315
Maximus <hey@m4xy.org>
2025-03-04 23:08:19 +0530

    Refactor URL routing and enhance templates; add about and booking pages with improved structure

--- workplace/littlelemon/littlelemon/urls.py
+++ workplace/littlelemon/littlelemon/urls.py
@@ -1,14 +1,21 @@
-from rest_framework.authtoken.views import obtain_auth_token
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from restaurant import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('restaurant/', include('restaurant.urls')),
+    path('restaurant/booking/', include('restaurant.urls')),
+    path('restaurant/menu/', include('restaurant.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    # Add root paths
     path('', views.index, name="home"),
     path('about/', views.about, name="about"),
     path('book/', views.book, name="book"), 
     path('menu/', views.menu, name="menu"),
     path('menu/<int:pk>/', views.display_menu_item, name="menu_item"),
-    path('menu/items/', views.MenuItemsView.as_view(), name='menu-items'),
-    path('menu/items/<int:pk>/', views.SingleMenuItemView.as_view(), name='single-menu-item'),
-    path('api-token-auth/', obtain_auth_token),
+    # API endpoints
+    path('api/menu/', views.MenuItemsView.as_view()),
+    path('api/menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
